de921e3b7bc91834754915a69122b6a3e596a27c
Stanislav Kravchuk <krauchuk.stanislau@securitylab.by>
2023-04-08 22:34:50 +0300

    second issue

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -182,6 +183,12 @@ class EventsController extends BaseController
      */
 
     public function getFutureEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 2');
+        $data = Event::whereHas('workshops', function ($query) {
+            $query->whereDate('start', '>=', Carbon::now());
+        })
+            ->with('workshops')
+            ->get();
+        return response()->json($data);
+//        throw new \Exception('implement in coding task 2');
     }
 }
